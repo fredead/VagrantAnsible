8556b6963bd04687a6a81368b743c16a4fd0677c
Simon Loader <simon@loaders.net>
2016-05-09 11:12:57 +0100

    Change hellow world to use name argument if added

--- helloWorld.go
+++ helloWorld.go
@@ -4,10 +4,16 @@ import (
         "net/http"
         "os"
 )
+
 func handler(w http.ResponseWriter, r *http.Request) {
         h, _ := os.Hostname()
-        fmt.Fprintf(w, "Hi there, I'm served from %s!", h)
+	if len(r.URL.Query().Get("name")) <= 0  {
+       	   fmt.Fprintf(w, "Hi there, I'm served from %s!", h)
+        } else {
+       	   fmt.Fprintf(w, "Hi there %s, I'm served from %s!", r.URL.Query().Get("name"), h)
+        }
 }
+
 func main() {
         http.HandleFunc("/", handler)
         http.ListenAndServe(":8484", nil)
